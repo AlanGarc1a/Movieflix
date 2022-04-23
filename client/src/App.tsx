fc9f7b8234633518a408e52b1353114a1da885b3
AlanGarc1a <alangarcia969@gmail.com>
2022-04-23 16:43:39 -0700

    added page route components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,32 @@
-import React from "react";
-import GlobalStyle from "./globalStyles";
-import Sidebar from "./components/Sidebar/Sidebar";
+import React from 'react';
+import GlobalStyle from './globalStyles';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import Discover from './pages/Discover';
+import Home from './pages/Home';
+import PopularMovies from './pages/PopularMovies';
+import PopularShows from './pages/PopularShows';
+import View from './pages/View';
+import MovieSearch from './pages/MovieSearch';
+import ShowSearch from './pages/ShowSearch';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Sidebar />
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='discover' element={<Discover />} />
+          <Route path='popular-movies' element={<PopularMovies />} />
+          <Route path='popular-shows' element={<PopularShows />} />
+          <Route path=':movieTitle/:movieId' element={<View />} />
+          <Route path='movie-search' element={<MovieSearch />} />
+          <Route path='tv-search' element={<ShowSearch />} />
+          <Route path='*' element={<NotFound />} />
+        </Route>
+      </Routes>
     </>
   );
 }
