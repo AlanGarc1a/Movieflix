62cea67f90d462075b9894aa284487747a7bf139
AlanGarc1a <alangarcia969@gmail.com>
2022-04-23 16:46:59 -0700

    created Grid component

---
+++ client/src/components/Grid/Grid.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Grid = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  flex-direction: row;
+  flex-wrap: wrap;
+  gap: 9rem 2rem;
+`;
