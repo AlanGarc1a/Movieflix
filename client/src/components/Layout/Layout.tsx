3ed1b857fc0020d1847c634231f31a85e5afbe8a
AlanGarc1a <alangarcia969@gmail.com>
2022-04-23 16:49:08 -0700

    added sidebar component and open menu state

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
+import Sidebar from '../Sidebar/Sidebar';
 
 const Layout: React.FC = () => {
+  const [openMenu, setOpenMenu] = useState<boolean>(false);
+
+  const toggle = () => {
+    setOpenMenu(!openMenu);
+  };
+
   return (
     <>
-      <Navbar />
+      <Navbar toggle={toggle} />
+      <Sidebar isOpen={openMenu} toggle={toggle} />
       <main>
         <Outlet />
       </main>
