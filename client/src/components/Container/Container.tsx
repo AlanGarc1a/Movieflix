8980f4b1751967a1811a148fe804b5a6328810d3
AlanGarc1a <alangarcia969@gmail.com>
2022-04-23 16:46:36 -0700

    created Container component

--- client/src/components/Container/Container.tsx
+++ client/src/components/Container/Container.tsx
@@ -6,6 +6,15 @@ const Wrapper = styled.div`
   margin: 5rem auto;
   padding-left: 3rem;
   padding-right: 3rem;
+
+  @media only screen and (max-width: 30em) {
+    padding: 0rem;
+    margin: 0;
+  }
+  @media only screen and (min-width: 48em) and (max-width: 50.625em) {
+    padding: 0rem;
+    margin: 0;
+  }
 `;
 
 const Container: React.FC = ({ children }) => {
