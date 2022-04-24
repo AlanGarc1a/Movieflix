afca306f87f3a191c509feea76ef5bc41fb04bf5
AlanGarc1a <alangarcia969@gmail.com>
2022-04-24 14:03:55 -0700

    serve build when in production

--- app.js
+++ app.js
@@ -13,6 +13,13 @@ app.use(express.json());
 
 app.use('/api', movieRoutes);
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('build'));
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
+}
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
