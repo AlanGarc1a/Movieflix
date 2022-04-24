813d9b8dd9e59586864f8672a71ff31632814173
AlanGarc1a <alangarcia969@gmail.com>
2022-04-23 17:00:55 -0700

    center content on smaller devices

--- client/src/components/NotFound/NoutFound.styles.tsx
+++ client/src/components/NotFound/NoutFound.styles.tsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 
 export const NFound = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   font-size: 8rem;
   font-weight: 700;
   color: var(--primary);
@@ -10,6 +13,14 @@ export const NFound = styled.div`
 export const NFoundCenter = styled.div`
   position: absolute;
   top: 30%;
-  left: 21%;
   transform: translateX(-50%, -50%);
+
+  @media only screen and (max-width: 30em) {
+    font-size: 4rem;
+    left: 0;
+  }
+
+  @media only screen and (max-width: 48em) {
+    font-size: 4rem;
+  }
 `;
