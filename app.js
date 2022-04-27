6eb0fb6eab3ce9ce462b2b90677de92b552bd211
AlanGarc1a <alangarcia969@gmail.com>
2022-04-26 17:34:05 -0700

    included path module

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ if (process.env.NODE_ENV === 'development') {
 }
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
