377fc34cc12d0ca2537de5de4bd0fc93cfce1608
AlanGarc1a <alangarcia969@gmail.com>
2022-05-06 15:51:47 -0700

    created user model

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const Schema = mongoose.Schema;
+
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+}
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
