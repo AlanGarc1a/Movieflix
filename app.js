8ed613f9ed3d7c7e8783e6e41ff532454099c8df
AlanGarc1a <alangarcia969@gmail.com>
2022-05-06 15:54:51 -0700

    connect to mongodb, add cors onfig obj, and user routes

--- app.js
+++ app.js
@@ -2,17 +2,33 @@ if (process.env.NODE_ENV === 'development') {
     require('dotenv').config();
 }
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
+const DB_URL = process.env.MONGO_URI || process.env.DB_TEST_URL;
+
+mongoose.connect(DB_URL)
+    .then(() => console.log('Database connected'))
+    .catch(() => console.log('Error connecting to database'));
+
 
 const movieRoutes = require('./routes/movieRoutes');
+const userRoutes = require('./routes/userRoutes');
 
-app.use(cors());
+const config = {
+    origin: 'http://localhost:3000',
+    credentials: true
+}
+
+app.use(cors(config));
+app.use(cookieParser());
 app.use(express.json());
 
 app.use('/api', movieRoutes);
+app.use('/api/users', userRoutes);
 
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, 'client/build')));
