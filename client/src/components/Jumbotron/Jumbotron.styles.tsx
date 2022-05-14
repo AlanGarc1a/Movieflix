fd79edb20a117e5a64ca6b5051db8bbf239b7679
AlanGarc1a <alangarcia969@gmail.com>
2022-05-14 12:16:30 -0700

    add dark jumbo styles

--- client/src/components/Jumbotron/Jumbotron.styles.tsx
+++ client/src/components/Jumbotron/Jumbotron.styles.tsx
@@ -40,8 +40,37 @@ export const JumboOverlay = styled.div`
     left: 0;
     width: 100%;
     height: 100%;
-    background: linear-gradient(180deg, #040303, #5c5c5c);
+    background: linear-gradient(180deg, #040303, #303f4c);
     mix-blend-mode: overlay;
-    opacity: 0.8;
+    opacity: 0.6;
+  }
+`;
+
+export const DarkJumbo = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  min-height: 55vh;
+  overflow: hidden;
+  top: 2%;
+
+  &:before {
+    content: '';
+    position: absolute;
+    top: 5%;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 0.5rem;
+    background-color: var(--primary);
+    background-size: cover;
+    background-position: center;
+  }
+
+  @media only screen and (max-width: 30em) {
+    height: 100vh;
+    top: -5%;
   }
 `;
